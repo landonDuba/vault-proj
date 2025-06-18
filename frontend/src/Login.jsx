508117b794011ac2600a73900a7194a7f2afdf1a
Landon <landonduba@vt.edu>
2025-06-17 23:35:21 -0400

    started login page

---
+++ frontend/src/Login.jsx
@@ -0,0 +1,13 @@
+import './Login.css'
+
+export default function Login() {
+    return (
+        <>
+        <h2>Welcome to Memory Vault</h2>
+        <div className="login-btn-group">
+            <button className='login-btn'>Login</button>
+            <button className='login-btn'>Sign Up</button>
+        </div>
+        </>
+    );
+}
