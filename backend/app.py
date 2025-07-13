d1afde3af3aaf1fcaea37ebe1573bbde083a7162
Landon <landonduba@vt.edu>
2025-07-12 22:34:39 -0400

    worked on making user Model

--- backend/app.py
+++ backend/app.py
@@ -1,1 +1,15 @@
-import flask
+from flask import Flask
+from models import db, User
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///C:/Users/lando/VaultProj/vault-proj/backend/database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
+
+
+if __name__ == '__main__':
+    with app.app_context():
+        print(db.engines)
+        db.create_all()
+    app.run(debug=True)
